e95939545db14899069ef73a7c280fcd89e68043
Miguel Brito <miguelbrit@gmail.com>
2026-02-27 13:19:56 -0400

    Fix: add missing useLocation import in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const MobileMenu = ({ isOpen, onClose, navLinks, location }) => {
   useEffect(() => {
